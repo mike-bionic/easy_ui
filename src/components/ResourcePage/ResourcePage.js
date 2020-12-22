4d373ba9947954eb555fbb9087736dacbc7d93c7
Mike Bionic <muhammedjepbarov@gmail.com>
2020-12-22 15:21:10 +0500

    element property pattern added

--- src/components/ResourcePage/ResourcePage.js
+++ src/components/ResourcePage/ResourcePage.js
@@ -7,6 +7,21 @@ import ItemDetails from '../ItemDetails'
 import ErrorIndicator from '../ErrorIndicator'
 import SapApiService from '../../services/SapApiService'
 
+
+const Row = ({left, right}) => {
+	return (
+		<div className="row mb2 ResourcePage">
+			<div className="col-md-6">
+				{left}
+			</div>
+			<div className="col-md-6">
+				{right}
+			</div>
+		</div>
+	)
+}
+
+
 class ResourcePage extends Component {
 
 	sapApi = new SapApiService()
@@ -27,23 +42,25 @@ class ResourcePage extends Component {
 		})
 	}
 
+	
 	render(){
 		if (this.state.hasError){
 			return <ErrorIndicator />
 		}
+
+		const itemList = (
+			<ItemList 
+				onItemSelected={this.onResourceSelected}
+				getData={this.sapApi.getResources}
+				renderItem={({id, name, ResPriceValue, ResCatName}) => (
+					`${name} (${ResPriceValue} TMT | ${ResCatName})`)} />
+		)
+		
+		const itemDetails = (
+			<ItemDetails resourceId={this.state.selectedResource} />
+		)
 		return (
-			<div className="row mb2 ResourcePage">
-				<div className="col-md-6">
-					<ItemList 
-						onItemSelected={this.onResourceSelected}
-						getData={this.sapApi.getResources}
-						renderItem={({id, name, ResPriceValue, ResCatName}) => (
-							`${name} (${ResPriceValue} TMT | ${ResCatName})`)} />
-				</div>
-				<div className="col-md-6">
-					<ItemDetails resourceId={this.state.selectedResource} />
-				</div>
-			</div>
+			<Row left={itemList} right={itemDetails} />
 		)
 	}
 }
