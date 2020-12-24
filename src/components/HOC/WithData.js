00159579997b7449de7bb6db947919b5382925f9
Mike Bionic <muhammedjepbarov@gmail.com>
2020-12-24 13:48:02 +0500

    updated HOC with error handler

--- src/components/HOC/WithData.js
+++ src/components/HOC/WithData.js
@@ -6,34 +6,46 @@ import ErrorIndicator from '../ErrorIndicator'
 const withData = (View, getData) => {
 	return class extends Component {
 		state = {
-			resourceList: null
+			data: null,
+			error: false,
+			loading: true
 		}
 	
 		onError = (err) => {
+			console.log(err)
 			this.setState({
 				error: true,
 				loading: false
 			})
+			console.log(this.state)
 		}
 		
 		componentDidMount() {
 	
 			getData()
 				.then((data) => {
-						this.setState({
-							data
-						})
+					this.setState({
+						data,
+						loading: false
 					})
-					.catch(this.onError)
+				})
+				.catch(this.onError)
 		}
 	
 		render() {
-			const {data} = this.state
-			if (!data) {
-				return <Spinner />
-			}
-	
-			return <View {...this.props} data={data} />
+			const {data, loading, error} = this.state
+
+			const errorView = error ? <ErrorIndicator /> : null
+			const spinner = loading ? <Spinner /> : null
+			const itemView = !(error || loading) ? <View {...this.props} data={data} /> : null
+
+			return (
+				<React.Fragment>
+					{errorView}
+					{spinner}
+					{itemView}
+				</React.Fragment>
+			)
 		}
 	}
 }
