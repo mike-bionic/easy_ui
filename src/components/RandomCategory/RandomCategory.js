b09a3e4a3abcb0b7f14d83184992729f13d338d9
Mike Bionic <muhammedjepbarov@gmail.com>
2020-12-30 15:28:50 +0500

    used defaultProps and propTypes

--- src/components/RandomCategory/RandomCategory.js
+++ src/components/RandomCategory/RandomCategory.js
@@ -8,98 +8,113 @@ import './RandomCategory.css'
 
 class RandomCategory extends Component {
 
-  state = {
-    category: {},
-    loading: true,
-    error: false
-  }
+	static defaultProps = {
+		updateInterval: 3500
+	}
 
-  sapApi = new SapApiService();
+	static propTypes = {
+		updateInterval: (props, propName, componentName) => {
+			const value = props[propName]
+			if (typeof value === 'number' && !isNaN(value)) {
+				return null
+			}
+			return new TypeError(`${componentName}: ${propName} must be number`)
+		}
+	}
 
-  componentDidMount() {
-    this.updateCategory();
-    this.interval = setInterval(this.updateCategory,3500)
-  }
+	state = {
+		category: {},
+		loading: true,
+		error: false
+	}
 
-  componentWillUnmount() {
-    clearInterval(this.interval)
-  }
+	sapApi = new SapApiService();
 
-  onError = (err) => {
-    this.setState({
-      error: true,
-      loading: false
-    })
-  }
+	componentDidMount() {
+		const {updateInterval} = this.props
+		this.updateCategory();
+		this.interval = setInterval(this.updateCategory,updateInterval)
+	}
 
-  onCategoryLoaded = (category) => {
-    this.setState({
-      category,
-      loading: false
-    });
-  }
+	componentWillUnmount() {
+		clearInterval(this.interval)
+	}
 
-  updateCategory = () => {
-    const ResCatId = Math.floor(Math.random()*8) + 2
-    this.sapApi
-      .getCategory(ResCatId)
-      .then(this.onCategoryLoaded)
-      .catch(this.onError)
-  }
+	onError = (err) => {
+		this.setState({
+			error: true,
+			loading: false
+		})
+	}
 
-  render() {
-    const {category, loading, error} = this.state
+	onCategoryLoaded = (category) => {
+		this.setState({
+			category,
+			loading: false
+		});
+	}
 
-    const errorView = error ? <ErrorIndicator /> : null
-    const spinner = loading ? <Spinner /> : null
-    const categoryView = !(loading || error) ? <CategoryView category={category} /> : null
-    
-    return (
-      <div className="RandomCategory jumbotron rounded">
-        {spinner}
-        {categoryView}
-        {errorView}
-      </div>
-    )
-  }
+	updateCategory = () => {
+		const ResCatId = Math.floor(Math.random()*8) + 2
+		this.sapApi
+			.getCategory(ResCatId)
+			.then(this.onCategoryLoaded)
+			.catch(this.onError)
+	}
+
+	render() {
+		const {category, loading, error} = this.state
+
+		const errorView = error ? <ErrorIndicator /> : null
+		const spinner = loading ? <Spinner /> : null
+		const categoryView = !(loading || error) ? <CategoryView category={category} /> : null
+		
+		return (
+			<div className="RandomCategory jumbotron rounded">
+				{spinner}
+				{categoryView}
+				{errorView}
+			</div>
+		)
+	}
 }
 
 const CategoryView = ({category}) => {
 
-  const {
-    id,
-    name,
-    description,
-    visibleIndex,
-    createdDate,
-    image
-  } = category
+	const {
+		id,
+		name,
+		description,
+		visibleIndex,
+		createdDate,
+		image
+	} = category
 
 
-  return (
-    <React.Fragment>
-      <img 
-          src={image}
-          alt="Category" />
-      <div>
-        <h4>{name}</h4>
-        <ul className="list-group list-group-flush">
-          <li className="list-group-item">
-            <span className="term">Description</span>
-            <span>{description}</span>
-          </li>
-          <li className="list-group-item">
-            <span className="term">Visible index</span>
-            <span>{visibleIndex}</span>
-          </li>
-          <li className="list-group-item">
-            <span className="term">Date</span>
-            <span>{createdDate}</span>
-          </li>
-        </ul>
-      </div>
-    </React.Fragment>
-  )
+	return (
+		<React.Fragment>
+			<img 
+					src={image}
+					alt="Category" />
+			<div>
+				<h4>{name}</h4>
+				<ul className="list-group list-group-flush">
+					<li className="list-group-item">
+						<span className="term">Description</span>
+						<span>{description}</span>
+					</li>
+					<li className="list-group-item">
+						<span className="term">Visible index</span>
+						<span>{visibleIndex}</span>
+					</li>
+					<li className="list-group-item">
+						<span className="term">Date</span>
+						<span>{createdDate}</span>
+					</li>
+				</ul>
+			</div>
+		</React.Fragment>
+	)
 }
 
 export default RandomCategory
